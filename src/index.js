90fcb5b89af713717cfc78cc1972258bd9527aca
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-12-16 13:02:07 +0100

    changing module for body parsing to express built in metod

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
@@ -7,7 +6,7 @@ const app = express();      // Express-app
 
 
 
-app.use(bodyParser.json());   // Middleware hantera i kommande requests json-data 
+app.use(express.json());
 
 
 app.use(cors());   // Middleware Tillåta begräsningar från andra domäner
