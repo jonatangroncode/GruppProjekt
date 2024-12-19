85958e369aa34af5cf074f29a82f4e49fd3e9a64
natnael_94@hotmail.com <natnael_94@hotmail.com>
2024-12-19 11:47:42 +0100

    push

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/user", userRoutes);
 
 app.use((req, res) => res.status(404).json({ message: "Route not found." }));
 app.use((err, req, res, next) => {
