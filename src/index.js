215fe0f2365be5eafbacc14ff2236c99e5f1400c
Renée Gladys <renee16@live.se>
2024-12-19 11:59:29 +0100

    trying connect mongo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db')
 
-=======
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
->>>>>>> f3bc4bd9916ebff4b591450cb679eaac144f3440
 require("dotenv").config();
 
 const app = express();
@@ -18,21 +12,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-
-app.use(cors());   // Middleware Tillåta begräsningar från andra domäner
-
-connectDB ();
 
 // Get-rut för startsidan
 app.get('/', (req, res) => {
    res.send('Välkommen till Express-servern!');
-=======
+
 app.use((req, res) => res.status(404).json({ message: "Route not found." }));
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ message: "Internal server error." });
->>>>>>> f3bc4bd9916ebff4b591450cb679eaac144f3440
 });
 
 app.listen(PORT, () =>
